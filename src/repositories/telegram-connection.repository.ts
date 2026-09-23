551b88e1b3c20127d311e5e0d537d0d505634151
Pedro Henrique <contatopedrohq@gmail.com>
2026-09-23 06:13:33 -0300

    feat: implement update  method in telegram connection repository

--- src/repositories/telegram-connection.repository.ts
+++ src/repositories/telegram-connection.repository.ts
@@ -2,11 +2,18 @@ import { pool } from '../config/database';
 import { Telegram } from '../types/telegram.types';
 
 
-export async function create(telegram: Omit<Telegram, 'chatId' | 'confirmed' | 'id' | 'createdAt'>): Promise<number> {
+export async function createTelegramConnection(telegram: Omit<Telegram, 'chatId' | 'confirmed' | 'id' | 'createdAt'>): Promise<number> {
  const [result] = await pool.query(
     'INSERT INTO telegram_connections (connection_code) VALUES (?)',
     [telegram.connectionCode]
  );
     const insertResult = result as { insertId: number };
     return insertResult.insertId;
+}
+
+export async function updateTelegramConnection(connectionCode: string, chatId: number,): Promise<void> {
+  await pool.query(
+    'UPDATE telegram_connections SET chat_id = ?, confirmed = true WHERE connection_code = ?',
+    [chatId, connectionCode]
+  );
 }
